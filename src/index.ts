6659049dce7d09a92f002448ea79768a9baa2e5d
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-03 22:36:27 +0600

    first type script code

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let sales: number = 123_545_455;
+let course : string = "TypeScript";
+let is_published: boolean = true;
+let level;
+
+function render (document:any){
+    console.log(document)
+}
+
